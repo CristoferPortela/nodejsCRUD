02cac5ad9cf4b1cbea2e2c0e563326937fc6b222
CristoferPortela <cristoferportela@gmail.com>
2020-03-24 14:07:03 -0300

    more routes http method added to receive more functionalities

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,13 @@
 // here we'll put all the routes
 
 module.exports = app => {
+    const api = app.api
     // the users crud route
     app.route('/users')
-        .post(app.src.api.user.create) // comming from consig
+        .post(api.user.create) // comming from consig
+        .get(api.user.get)
+    
+    app.route('/users/:id')
+        .put(api.user.create)
+        .get(api.user.getById)
 }
