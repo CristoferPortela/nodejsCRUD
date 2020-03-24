3a99fb570775ac58da5b6347451be3900e27b78c
CristoferPortela <cristoferportela@gmail.com>
2020-03-24 14:06:25 -0300

    consign fixed, cwd added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 'use strict'
 const app = require('express')()
+// consign will possibility us to seperate the files easier
 const consign = require('consign')
+// the knex config
+const env = require('./config/env')
 
-consign()
-    .then('src/config/middleware.js')
-    .then('src/api')
-    .then('src/config/routes.js')
+// add the knex to app, so the knex will be avaible in knex
+app.env = env
+
+consign({ cwd: 'src' })
+    .then('config/middleware.js')
+    .then('api/validation.js')
+    .then('api')
+    .then('config/routes.js')
     .into(app)
 
 // port to execute
